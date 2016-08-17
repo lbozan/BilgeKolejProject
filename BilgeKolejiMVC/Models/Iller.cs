9bc97fbd2434e8dfd613d95d3bb2728707f958ce
LBozan <lbozan@outlook.com>
2016-08-17 22:36:27 +0300

    Create Iller.cs

---
+++ BilgeKolejiMVC/Models/Iller.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+    
+namespace BilgeKolejProject
+{
+    public  class Iller
+    {
+        public int ID { get; set; }
+        public string IlAdi { get; set; }
+        public Nullable<System.DateTime> Tarih { get; set; }
+    }
+}
