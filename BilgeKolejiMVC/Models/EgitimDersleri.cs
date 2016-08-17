4487669baf485440c7cba3b1b5482070e0eaa6e8
LBozan <lbozan@outlook.com>
2016-08-17 22:37:27 +0300

    Create EgitimDersleri.cs

---
+++ BilgeKolejiMVC/Models/EgitimDersleri.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+    
+namespace BilgeKolejProject
+{
+    public  class EgitimDersleri
+    {
+        public int ID { get; set; }
+        public Nullable<int> UstID { get; set; }
+        public string DersAdi { get; set; }
+        public string DersKodu { get; set; }
+        public int Hd { get; set; }
+        public int Sirasi { get; set; }
+    }
+}
