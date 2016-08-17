d123125488971ccf4df51c1e8ff9e2a0182c28be
LBozan <lbozan@outlook.com>
2016-08-17 22:23:31 +0300

    Create Branslar.cs

---
+++ BilgeKolejiMVC/Models/Branslar.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+    
+namespace BilgeKolejProject
+{
+    public  class Branslar
+    {
+        public int ID { get; set; }
+        public string BransAdi { get; set; }
+        public bool? Aktifmi { get; set; }
+        public DateTime? Tarih { get; set; }
+    }
+}
