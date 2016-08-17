241bacab34e6d0f2ce6807fb89979b04fcc7deac
LBozan <lbozan@outlook.com>
2016-08-17 22:31:42 +0300

    Create OgretmenDurum.cs

---
+++ BilgeKolejiMVC/Models/OgretmenDurum.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+    
+namespace BilgeKolejProject
+{
+    public  class OgretmenDurum
+    {
+        public int ID { get; set; }
+        public string DurumAdi { get; set; }
+        public Nullable<bool> Aktifmi { get; set; }
+        public Nullable<System.DateTime> Tarih { get; set; }
+    }
+}
