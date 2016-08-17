f23629f2c4bd66f8392072ef87fac71b80c044f1
LBozan <lbozan@outlook.com>
2016-08-17 22:22:25 +0300

    Create BitirdigiOkul.cs

---
+++ BilgeKolejiMVC/Models/BitirdigiOkul.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace BilgeKolejProject
+{
+    public  class BitirdigiOkul
+    {
+        public int ID { get; set; }
+        public string OkulAdi { get; set; }
+        public DateTime Tarih { get; set; }
+    }
+}
