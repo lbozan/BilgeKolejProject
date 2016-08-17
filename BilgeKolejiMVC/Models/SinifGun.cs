3f6b22b7e657a9275e5bfda74e2f9ae9f1b6947d
LBozan <lbozan@outlook.com>
2016-08-17 22:28:34 +0300

    Update SinifGun.cs

--- BilgeKolejiMVC/Models/SinifGun.cs
+++ BilgeKolejiMVC/Models/SinifGun.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 
 namespace BilgeKolejProject
 {
-        public int ID { get; set; }
-        public string GunAdi { get; set; }
-     
+       public  class SinifGun
+        {
+                public int ID { get; set; }
+                public string GunAdi { get; set; }
+        }
 }
